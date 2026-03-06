a50692457b49452591e0d8f6a59b1e3c47eb3a13
thanhson1612208 <thanhson1612208@gmail.com>
2026-03-06 21:10:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ client.once('ready', () => {
 client.on('messageCreate', message => {
   
   if (message.author.bot) return;
-  if (message.content === "/pay") {
+  if (message.content === "!pay") {
   message.channel.send({
     content: "💳 **THÔNG TIN THANH TOÁN**\n\n🏦 Ngân hàng: Vietcombank\n👤 Chủ TK: BUI THANH SON\n🔢 STK: 1944627277\n\n📌 Nội dung CK: Discord + dịch vụ mua\n\nSau khi chuyển khoản hãy gửi bill cho admin để xác nhận."
   });
 }
-  if (message.content === '/menu') {
+  if (message.content === '!menu') {
     message.channel.send({
       content: "📜 **Bảng Giá Dexsty Shop**",
       files: ["./Messenger_creation_214E610D-DB3C-4EA3-9455-2650B4663371.jpeg"]
