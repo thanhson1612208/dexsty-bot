2c892a71f954d9dd6924b39257230e41060e9870
thanhson1612208 <thanhson1612208@gmail.com>
2026-03-06 23:45:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -43,12 +43,29 @@ client.on('messageCreate', message => {
     content: "💳 **THÔNG TIN THANH TOÁN**\n\n🏦 Ngân hàng: Vietcombank\n👤 Chủ TK: BUI THANH SON\n🔢 STK: 1944627277\n\n📌 Nội dung CK: Discord + dịch vụ mua\n\nSau khi chuyển khoản hãy gửi bill cho admin để xác nhận."
   });
 }
-  if (message.content === '!menu') {
-    message.channel.send({
-      content: "📜 **Bảng Giá Dexsty Shop**",
-      files: ["./Messenger_creation_214E610D-DB3C-4EA3-9455-2650B4663371.jpeg"]
-    });
-  }
+  const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+if (message.content === "!menu") {
+
+const row = new ActionRowBuilder()
+.addComponents(
+new ButtonBuilder()
+.setCustomId("buy")
+.setLabel("🛒 Mua dịch vụ")
+.setStyle(ButtonStyle.Primary),
+
+new ButtonBuilder()
+.setCustomId("pay")
+.setLabel("💳 Thanh toán")
+.setStyle(ButtonStyle.Success)
+);
+
+message.channel.send({
+content: "📜 **MENU DỊCH VỤ BLOX FRUIT - DEXSTY**",
+files: ["menu.jpg"],
+components: [row]
 });
+
+}
 
 client.login(process.env.TOKEN);
