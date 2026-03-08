7d5e3170fb3d7d50ab9c65232fdbcd0bfa086ae4
thanhson1612208 <thanhson1612208@gmail.com>
2026-03-09 01:26:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, AttachmentBuilder } = require('discord.js');
-const { ActionRowBuilder, StringSelectMenuBuilder } = require("discord.js");
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
